72a3638b49f81643b9d1e566aadb84964ae9d824
baroiroi <johnroy0414@gmail.com>
2024-02-27 14:28:33 +0800

    mui practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { MuiButton } from './components/MuiButton';
-import { MuiTypography } from './components/MuiTypography';
+// import { MuiTypography } from './components/MuiTypography';
 
 function App() {
   return (
