81777de451178fca4a250853921c367e49259998
baroiroi <johnroy0414@gmail.com>
2024-02-28 14:13:58 +0800

    ToggleButtonGroup Props and handleChange

--- src/components/MuiButton.tsx
+++ src/components/MuiButton.tsx
@@ -1,7 +1,16 @@
-import { Stack, Button, IconButton } from '@mui/material'
+import { Stack, Button, IconButton, ButtonGroup, ToggleButtonGroup, ToggleButton } from '@mui/material'
 import SendIcon from '@mui/icons-material/Send';
+import FormatBoldIcon from '@mui/icons-material/FormatBold';
+import FormatItalicIcon from '@mui/icons-material/FormatItalic';
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined'
+import { useState } from 'react';
 
 export const MuiButton = () => {
+  const [format, setFormat] = useState<string | null>(null);
+  console.log({format,});
+  const handleFormatChange = (_event: React.MouseEvent<HTMLElement>, updatedFormat: string | null) => {
+    setFormat(updatedFormat)
+  };
   return (
   <Stack spacing={4}>
     <Stack spacing={2} direction='row'>
@@ -11,12 +20,12 @@ export const MuiButton = () => {
     </Stack>
 
     <Stack spacing={2} direction='row'>
-      <Button variant='contained' color='primary'>Primary</Button>
-      <Button variant='contained' color='secondary'>Secondary</Button>
-      <Button variant='contained' color='error'>Error</Button>
-      <Button variant='contained' color='warning'>Warning</Button>
-      <Button variant='contained' color='info'>Info</Button>
-      <Button variant='contained' color='success'>Success</Button>
+      <Button variant='outlined' color='primary'>Primary</Button>
+      <Button variant='outlined' color='secondary'>Secondary</Button>
+      <Button variant='outlined' color='error'>Error</Button>
+      <Button variant='outlined' color='warning'>Warning</Button>
+      <Button variant='outlined' color='info'>Info</Button>
+      <Button variant='outlined' color='success'>Success</Button>
     </Stack>
     
     <Stack display='block' spacing={2} direction='row'>
@@ -26,9 +35,36 @@ export const MuiButton = () => {
     </Stack>
 
     <Stack spacing={2} direction='row'>
-      <Button variant='contained' startIcon={<SendIcon />} disableRipple>Send</Button>
-      <Button variant='contained' endIcon={<SendIcon />} disableElevation>Send</Button>
+      <Button 
+      variant='outlined' 
+      startIcon={<SendIcon />} 
+      disableRipple
+      onClick={() => alert('Clicked')}>
+      Send
+      </Button>
+      <Button variant='outlined' endIcon={<SendIcon />} disableElevation>Send</Button>
       <IconButton aria-label='send' color='success' size='small'> <SendIcon /> </IconButton>
+    </Stack>
+    <Stack direction='row'>
+      <ButtonGroup 
+      variant='contained' 
+      disableRipple 
+      orientation='horizontal' 
+      size='large'
+      color='warning'
+      aria-label='alignment button group'
+      >
+      <Button onClick={() => alert('left clicked')}>Left</Button>
+      <Button onClick={() => alert('center clicked')}>Center</Button>
+      <Button onClick={() => alert('right clicked')}>Right</Button>
+      </ButtonGroup>
+    </Stack>
+    <Stack direction='row'>
+      <ToggleButtonGroup aria-label='text formatting' value={format} onChange={handleFormatChange} size='large' color='success' exclusive>
+        <ToggleButton value='bold' aria-label='bold' ><FormatBoldIcon /></ToggleButton>
+        <ToggleButton value='italic' aria-label='italic'><FormatItalicIcon /></ToggleButton>
+        <ToggleButton value='underlined' aria-label='underlined'><FormatUnderlinedIcon /></ToggleButton>
+      </ToggleButtonGroup>
     </Stack>
   </Stack>
   )
