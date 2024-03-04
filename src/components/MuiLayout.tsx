f1fa8e1331c5ef1eb90893d0c2913c2818524881
baroiroi <johnroy0414@gmail.com>
2024-03-04 14:02:14 +0800

    Grid Layout Sample

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -28,17 +28,17 @@ export const MuiLayout = () => {
         Noha
     </Box>
   </Stack>
-  <Grid container my={4}>
-    <Grid item xs={3}>
+  <Grid container my={4} rowSpacing={2} columnSpacing={1}>
+    <Grid item xs={6}>
       <Box bgcolor='primary.light' padding={2}>Item 1</Box>
     </Grid>
-    <Grid item xs={9}>
+    <Grid item xs={6}>
       <Box bgcolor='primary.light' padding={2}>Item 2</Box>
     </Grid>  
-    <Grid item xs={9}>
+    <Grid item xs={6}>
       <Box bgcolor='primary.light' padding={2}>Item 3</Box>
     </Grid>  
-    <Grid item xs={3}>
+    <Grid item xs={6}>
       <Box bgcolor='primary.light' padding={2}>Item 4</Box>
     </Grid>     
   </Grid>
