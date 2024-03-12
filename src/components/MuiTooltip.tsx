b60da6700d111be4fad4435fc18aff2085aab457
baroiroi <johnroy0414@gmail.com>
2024-03-12 10:19:40 +0800

    Mui Tooltip Component

---
+++ src/components/MuiTooltip.tsx
@@ -0,0 +1,13 @@
+import { Tooltip, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+export const MuiTooltip = () => {
+  return (
+    <Tooltip title='Delete' placement='top' arrow enterDelay={500} leaveDelay={1000}>
+        <IconButton>
+            <DeleteIcon />
+        </IconButton>
+    </Tooltip>
+  )
+}
