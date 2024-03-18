72491b590467e84db589a3a662ebf03cd5fd267d
baroiroi <johnroy0414@gmail.com>
2024-03-18 09:37:11 +0800

    Mui LoadingButton Component

--- src/components/MuiLoadingButton.tsx
+++ src/components/MuiLoadingButton.tsx
@@ -1,9 +1,28 @@
 import { Stack } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import { useState } from 'react';
+import SaveIcon from '@mui/icons-material/Save';
 
 export const MuiLoadingButton = () => {
   const [loading, setLoading] = useState(false);
+  const [saving, setSaving] = useState(false);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+  }, 13000);
+};
+
+const handleSave = (event: React.MouseEvent<HTMLElement>) => {
+  setSaving(true);
+
+  setTimeout(() => {
+    setSaving(false);
+}, 13000);
+};
+
+
   return (
     <Stack spacing={2} direction='row'>
         <LoadingButton variant='outlined'centerRipple>Submit</LoadingButton>
@@ -11,10 +30,17 @@ export const MuiLoadingButton = () => {
         <LoadingButton variant='outlined' loadingIndicator='loading...' centerRipple>Fetch Data</LoadingButton>
         <LoadingButton variant='outlined' loadingIndicator='loading...' loading>Fetch Data</LoadingButton>
         <LoadingButton variant='outlined'
-        loadingIndicator='loading'
         centerRipple
-        >Fetch Data</LoadingButton>
+        onClick={handleClick}>
+          {
+            loading ? 'Loading...' : 'Fetch Data'
+          }
+        </LoadingButton>
+        <LoadingButton variant='outlined' loadingIndicator='loading...' onClick={handleSave} loadingPosition='start'>
+          {
+            saving ? 'Saving...' : 'Save'
+          }
+        </LoadingButton>
     </Stack>
   )
 }
-
